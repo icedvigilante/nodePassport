26481210f2ef01b19df274e6cfce01b912a23baf
icedvigilante <wm.ryan.mooney@gmail.com>
2020-11-13 17:42:51 -0900

    Setup User Schema

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+
+// db config
+const db = require('./config/keys').MongoURI;
+
+//Connect to Mongo
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
 
 // EJS
 app.use(expressLayouts);
@@ -13,5 +22,5 @@ app.set('view engine', 'ejs')
 
 app.use('/', require('./routes/index'))
 app.use('/users', require('./routes/users'))
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on ${PORT}`));
